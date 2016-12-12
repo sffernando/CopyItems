e91d9d59bb8ebaa98de325f6d028571940cf8282
Fernando <sucrezhang@qq.com>
2016-12-12 18:18:22 +0800

    class ivar

--- CPYYModel/CPYYModel/CPYYModelSorce/YYClassInfo.m
+++ CPYYModel/CPYYModel/CPYYModelSorce/YYClassInfo.m
@@ -7,6 +7,108 @@
 //
 
 #import "YYClassInfo.h"
+#import <objc/runtime.h>
+
+YYEncodingType YYEncodingGetType(const char *typeEncoding) {
+    char *type = (char *)typeEncoding;
+    if (!type) return YYEncodingTypeUnknown;
+    size_t len = strlen(type);
+    if (len == 0) return YYEncodingTypeUnknown;
+    
+    YYEncodingType qualifier = 0;
+    bool prefix = true;
+    while (prefix) {
+        switch (*type) {
+            case 'r': {
+                qualifier |= YYEncodingTypeQualifierConst;
+                type++;
+            } break;
+            case 'n': {
+                qualifier |= YYEncodingTypeQualifierIn;
+                type++;
+            } break;
+            case 'N': {
+                qualifier |= YYEncodingTypeQualifierInout;
+                type++;
+            } break;
+            case 'o': {
+                qualifier |= YYEncodingTypeQualifierOut;
+                type++;
+            } break;
+            case 'O': {
+                qualifier |= YYEncodingTypeQualifierBycopy;
+                type++;
+            } break;
+            case 'R': {
+                qualifier |= YYEncodingTypeQualifierByref;
+                type++;
+            } break;
+            case 'V': {
+                qualifier |= YYEncodingTypeQualifierOneway;
+                type++;
+            } break;
+            default: { prefix = false; } break;
+        }
+    }
+    
+    len = strlen(type);
+    if (len == 0) return YYEncodingTypeUnknown | qualifier;
+    
+    switch (*type) {
+        case 'v': return YYEncodingTypeVoid | qualifier;
+        case 'B': return YYEncodingTypeBool | qualifier;
+        case 'c': return YYEncodingTypeInt8 | qualifier;
+        case 'C': return YYEncodingTypeUInt8 | qualifier;
+        case 's': return YYEncodingTypeInt16 | qualifier;
+        case 'S': return YYEncodingTypeUInt16 | qualifier;
+        case 'i': return YYEncodingTypeInt32 | qualifier;
+        case 'I': return YYEncodingTypeUInt32 | qualifier;
+        case 'l': return YYEncodingTypeInt32 | qualifier;
+        case 'L': return YYEncodingTypeUInt32 | qualifier;
+        case 'q': return YYEncodingTypeInt64 | qualifier;
+        case 'Q': return YYEncodingTypeUInt64 | qualifier;
+        case 'f': return YYEncodingTypeFloat | qualifier;
+        case 'd': return YYEncodingTypeDouble | qualifier;
+        case 'D': return YYEncodingTypeLongDouble | qualifier;
+        case '#': return YYEncodingTypeClass | qualifier;
+        case ':': return YYEncodingTypeSEL | qualifier;
+        case '*': return YYEncodingTypeCString | qualifier;
+        case '^': return YYEncodingTypePointer | qualifier;
+        case '[': return YYEncodingTypeCArray | qualifier;
+        case '(': return YYEncodingTypeUnion | qualifier;
+        case '{': return YYEncodingTypeStruct | qualifier;
+        case '@': {
+            if (len == 2 && *(type + 1) == '?')
+                return YYEncodingTypeBlock | qualifier;
+            else
+                return YYEncodingTypeObject | qualifier;
+        }
+        default: return YYEncodingTypeUnknown | qualifier;
+    }
+}
+
+@implementation YYClassIvarInfo
+
+- (instancetype)initWithIvar:(Ivar)ivar {
+    if (!ivar) {
+        return nil;
+    }
+    self = [super init];
+    _ivar = ivar;
+    const char *name = ivar_getName(ivar);
+    if (name) {
+        _name = [NSString stringWithUTF8String:name];
+    }
+    _offset = ivar_getOffset(ivar);
+    const char *typeEncoding = ivar_getTypeEncoding(ivar);
+    if (typeEncoding) {
+        _typeEncoding = [NSString stringWithUTF8String:typeEncoding];
+        _type = YYEncodingGetType(typeEncoding);
+    }
+    return self;
+}
+
+@end
 
 @implementation YYClassInfo
 
