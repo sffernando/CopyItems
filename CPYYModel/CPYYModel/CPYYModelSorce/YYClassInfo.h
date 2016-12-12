d00731e6d0529f92ce90dac6c437598395691a66
Fernando <sucrezhang@qq.com>
2016-12-12 15:11:21 +0800

    copy yymodel

--- CPYYModel/CPYYModel/CPYYModelSorce/YYClassInfo.h
+++ CPYYModel/CPYYModel/CPYYModelSorce/YYClassInfo.h
@@ -7,7 +7,96 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <objc/runtime.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+/**
+ Type encoding's type.
+ */
+typedef NS_OPTIONS(NSUInteger, YYEncodingType) {
+    YYEncodingTypeMask       = 0xFF, ///< mask of type value
+    YYEncodingTypeUnknown    = 0, ///< unknown
+    YYEncodingTypeVoid       = 1, ///< void
+    YYEncodingTypeBool       = 2, ///< bool
+    YYEncodingTypeInt8       = 3, ///< char / BOOL
+    YYEncodingTypeUInt8      = 4, ///< unsigned char
+    YYEncodingTypeInt16      = 5, ///< short
+    YYEncodingTypeUInt16     = 6, ///< unsigned short
+    YYEncodingTypeInt32      = 7, ///< int
+    YYEncodingTypeUInt32     = 8, ///< unsigned int
+    YYEncodingTypeInt64      = 9, ///< long long
+    YYEncodingTypeUInt64     = 10, ///< unsigned long long
+    YYEncodingTypeFloat      = 11, ///< float
+    YYEncodingTypeDouble     = 12, ///< double
+    YYEncodingTypeLongDouble = 13, ///< long double
+    YYEncodingTypeObject     = 14, ///< id
+    YYEncodingTypeClass      = 15, ///< class
+    YYEncodingTypeSEL        = 16, ///< SEL
+    YYEncodingTypeBlock      = 17, ///< block
+    YYEncodingTypePointer    = 18, ///< void*
+    YYEncodingTypeStruct     = 19, ///< struct
+    YYEncodingTypeUnion      = 20, ///< union
+    YYEncodingTypeCString    = 21, ///< char*
+    YYEncodingTypeCArray     = 22, ///< char[10] (for example)
+    
+    YYEncodingTypeQualifierMask   = 0xFF00,   ///< mask of qualifier
+    YYEncodingTypeQualifierConst  = 1 << 8,  ///< const
+    YYEncodingTypeQualifierIn     = 1 << 9,  ///< in
+    YYEncodingTypeQualifierInout  = 1 << 10, ///< inout
+    YYEncodingTypeQualifierOut    = 1 << 11, ///< out
+    YYEncodingTypeQualifierBycopy = 1 << 12, ///< bycopy
+    YYEncodingTypeQualifierByref  = 1 << 13, ///< byref
+    YYEncodingTypeQualifierOneway = 1 << 14, ///< oneway
+    
+    YYEncodingTypePropertyMask         = 0xFF0000, ///< mask of property
+    YYEncodingTypePropertyReadonly     = 1 << 16, ///< readonly
+    YYEncodingTypePropertyCopy         = 1 << 17, ///< copy
+    YYEncodingTypePropertyRetain       = 1 << 18, ///< retain
+    YYEncodingTypePropertyNonatomic    = 1 << 19, ///< nonatomic
+    YYEncodingTypePropertyWeak         = 1 << 20, ///< weak
+    YYEncodingTypePropertyCustomGetter = 1 << 21, ///< getter=
+    YYEncodingTypePropertyCustomSetter = 1 << 22, ///< setter=
+    YYEncodingTypePropertyDynamic      = 1 << 23, ///< @dynamic
+};
+
+
+/**
+ Get the type from a Type-Encoding string.
+ 
+ @discussion See also:
+ https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/ObjCRuntimeGuide/Articles/ocrtTypeEncodings.html
+ https://developer.apple.com/library/mac/documentation/Cocoa/Conceptual/ObjCRuntimeGuide/Articles/ocrtPropertyIntrospection.html
+
+ @param typeEncoding  A Type-Encoding string.
+ @return The encoding type.
+ */
+YYEncodingType YYEncodingGetType(const char *typeEncoding);
+
+
+/**
+ Instance variable information
+ */
+@interface YYClassIvarInfo : NSObject
+@property (nonatomic, assign, readonly) Ivar ivar;              ///< ivar opaque struct
+@property (nonatomic, strong, readonly) NSString *name;         ///< Ivar's name
+@property (nonatomic, assign, readonly) ptrdiff_t offset;       ///< Ivar's offset
+@property (nonatomic, strong, readonly) NSString *typeEncoding; ///< Ivar's type encoding
+@property (nonatomic, assign, readonly) YYEncodingType type;    ///< Ivar's type
+
+
+/**
+ Creates and returns an ivar info object.
+
+ @param ivar ivar opaque struct
+ @return A new object, or nil if an error occurs.
+ */
+- (instancetype)initWithIvar:(Ivar)ivar;
+
+@end
 
 @interface YYClassInfo : NSObject
 
 @end
+
+NS_ASSUME_NONNULL_END
