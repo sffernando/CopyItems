f6254aeef039f3a2ad8031eca91c69465abbf685
Fernando <sucrezhang@qq.com>
2016-12-13 22:43:44 +0800

    some inline methds

--- CPYYModel/CPYYModel/CPYYModelSorce/NSObject+YYModel.m
+++ CPYYModel/CPYYModel/CPYYModelSorce/NSObject+YYModel.m
@@ -12,6 +12,122 @@
 
 #define force_inline __inline__ __attribute__((always_inline))
 
+/// Foundation Class Type
+typedef NS_ENUM (NSUInteger, YYEncodingNSType) {
+    YYEncodingTypeNSUnknown = 0,
+    YYEncodingTypeNSString,
+    YYEncodingTypeNSMutableString,
+    YYEncodingTypeNSValue,
+    YYEncodingTypeNSNumber,
+    YYEncodingTypeNSDecimalNumber,
+    YYEncodingTypeNSData,
+    YYEncodingTypeNSMutableData,
+    YYEncodingTypeNSDate,
+    YYEncodingTypeNSURL,
+    YYEncodingTypeNSArray,
+    YYEncodingTypeNSMutableArray,
+    YYEncodingTypeNSDictionary,
+    YYEncodingTypeNSMutableDictionary,
+    YYEncodingTypeNSSet,
+    YYEncodingTypeNSMutableSet,
+};
+
+/// Get the Foundation class type from property info.
+static force_inline YYEncodingNSType YYClassGetNSType(Class cls) {
+    if (!cls) return YYEncodingTypeNSUnknown;
+    if ([cls isSubclassOfClass:[NSMutableString class]]) return YYEncodingTypeNSMutableString;
+    if ([cls isSubclassOfClass:[NSString class]]) return YYEncodingTypeNSString;
+    if ([cls isSubclassOfClass:[NSDecimalNumber class]]) return YYEncodingTypeNSDecimalNumber;
+    if ([cls isSubclassOfClass:[NSNumber class]]) return YYEncodingTypeNSNumber;
+    if ([cls isSubclassOfClass:[NSValue class]]) return YYEncodingTypeNSValue;
+    if ([cls isSubclassOfClass:[NSMutableData class]]) return YYEncodingTypeNSMutableData;
+    if ([cls isSubclassOfClass:[NSData class]]) return YYEncodingTypeNSData;
+    if ([cls isSubclassOfClass:[NSDate class]]) return YYEncodingTypeNSDate;
+    if ([cls isSubclassOfClass:[NSURL class]]) return YYEncodingTypeNSURL;
+    if ([cls isSubclassOfClass:[NSMutableArray class]]) return YYEncodingTypeNSMutableArray;
+    if ([cls isSubclassOfClass:[NSArray class]]) return YYEncodingTypeNSArray;
+    if ([cls isSubclassOfClass:[NSMutableDictionary class]]) return YYEncodingTypeNSMutableDictionary;
+    if ([cls isSubclassOfClass:[NSDictionary class]]) return YYEncodingTypeNSDictionary;
+    if ([cls isSubclassOfClass:[NSMutableSet class]]) return YYEncodingTypeNSMutableSet;
+    if ([cls isSubclassOfClass:[NSSet class]]) return YYEncodingTypeNSSet;
+    return YYEncodingTypeNSUnknown;
+}
+
+/// Whether the type is c number.
+static force_inline BOOL YYEncodingTypeIsCNumber(YYEncodingType type) {
+    switch (type & YYEncodingTypeMask) {
+        case YYEncodingTypeBool:
+        case YYEncodingTypeInt8:
+        case YYEncodingTypeUInt8:
+        case YYEncodingTypeInt16:
+        case YYEncodingTypeUInt16:
+        case YYEncodingTypeInt32:
+        case YYEncodingTypeUInt32:
+        case YYEncodingTypeInt64:
+        case YYEncodingTypeUInt64:
+        case YYEncodingTypeFloat:
+        case YYEncodingTypeDouble:
+        case YYEncodingTypeLongDouble: return YES;
+        default: return NO;
+    }
+}
+
+/// Parse a number value from 'id'
+static force_inline NSNumber *YYNSNumberCreateFromID(__unsafe_unretained id value) {
+    static NSCharacterSet *dot;
+    static NSDictionary *dic;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        dot = [NSCharacterSet characterSetWithRange:NSMakeRange('.', 1)];
+        dic = @{@"TRUE" :   @(YES),
+                @"True" :   @(YES),
+                @"true" :   @(YES),
+                @"FALSE" :  @(NO),
+                @"False" :  @(NO),
+                @"false" :  @(NO),
+                @"YES" :    @(YES),
+                @"Yes" :    @(YES),
+                @"yes" :    @(YES),
+                @"NO" :     @(NO),
+                @"No" :     @(NO),
+                @"no" :     @(NO),
+                @"NIL" :    (id)kCFNull,
+                @"Nil" :    (id)kCFNull,
+                @"nil" :    (id)kCFNull,
+                @"NULL" :   (id)kCFNull,
+                @"Null" :   (id)kCFNull,
+                @"null" :   (id)kCFNull,
+                @"(NULL)" : (id)kCFNull,
+                @"(Null)" : (id)kCFNull,
+                @"(null)" : (id)kCFNull,
+                @"<NULL>" : (id)kCFNull,
+                @"<Null>" : (id)kCFNull,
+                @"<null>" : (id)kCFNull};
+    });
+    
+    if (!value || value == (id)kCFNull) return nil;
+    if ([value isKindOfClass:[NSNumber class]]) return value;
+    if ([value isKindOfClass:[NSString class]]) {
+        NSNumber *num = dic[value];
+        if (num) {
+            if (num == (id)kCFNull) return nil;
+            return num;
+        }
+        if ([(NSString *)value rangeOfCharacterFromSet:dot].location != NSNotFound) {
+            const char *cstring = ((NSString *)value).UTF8String;
+            if (!cstring) return nil;
+            double num = atof(cstring);
+            if (isnan(num) || isinf(num)) return nil;
+            return @(num);
+        } else {
+            const char *cstring = ((NSString *)value).UTF8String;
+            if (!cstring) return nil;
+            return @(atoll(cstring));
+        }
+    }
+    return nil;
+}
+
 @implementation NSObject (YYModel)
 
 @end
