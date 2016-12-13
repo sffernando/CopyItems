008f4375521d634ad1dc069a8cea2512768fe52b
Fernando <sucrezhang@qq.com>
2016-12-13 17:54:28 +0800

    methods declarations

--- CPYYModel/CPYYModel/CPYYModelSorce/NSObject+YYModel.h
+++ CPYYModel/CPYYModel/CPYYModelSorce/NSObject+YYModel.h
@@ -37,6 +37,135 @@ NS_ASSUME_NONNULL_BEGIN
  */
 + (nullable instancetype)yy_modelWithJSON:(id)json;
 
+/**
+ Creates and returns a new instance of the receiver from a key-value dictionary.
+
+ @param dictionary  A key-value dictionary mapped to the instance's properties.
+ Any invalid key-value pair in dictionary will be ignored.
+ 
+ @return A new instance created from the dictionary, or nil if an error occurs.
+ 
+ @discussion The key in `dictionary` will mapped to the reciever's property name,
+ and the value will set to the property. If the value's type does not match the
+ property, this method will try to convert the value based on these rules:
+ 
+ `NSString` or `NSNumber` -> c number, such as BOOL, int, long, float, NSUInteger...
+ `NSString` -> NSDate, parsed with format "yyyy-MM-dd'T'HH:mm:ssZ", "yyyy-MM-dd HH:mm:ss" or "yyyy-MM-dd".
+ `NSString` -> NSURL.
+ `NSValue` -> struct or union, such as CGRect, CGSize, ...
+ `NSString` -> SEL, Class.
+ */
++ (nullable instancetype)yy_modelWithDictionary:(NSDictionary *)dictionary;
+
+/**
+ Set the receiver's properties with a json object.
+
+ @discussion Any invalid data in json will be ignored.
+ 
+ @param json  A json object of `NSDictionary`, `NSString` or `NSData`, mapped to the
+ receiver's properties
+ 
+ @return Whether succeed
+ */
+- (BOOL)yy_modelSetWithJSON:(id)json;
+
+/**
+ Set the receiver's properties with a key-value dictionary.
+
+ @param dic  A key-value dictionary mapped to the receiver's properties.
+ Any invalid key-value pair in dictionary will be ignored.
+ 
+ @discussion The key in `dictionary` will mapped to the reciever's property name,
+ and the value will set to the property. If the value's type doesn't match the
+ property, this method will try to convert the value based on these rules:
+ 
+ `NSString`, `NSNumber` -> c number, such as BOOL, int, long, float, NSUInteger...
+ `NSString` -> NSDate, parsed with format "yyyy-MM-dd'T'HH:mm:ssZ", "yyyy-MM-dd HH:mm:ss" or "yyyy-MM-dd".
+ `NSString` -> NSURL.
+ `NSValue` -> struct or union, such as CGRect, CGSize, ...
+ `NSString` -> SEL, Class.
+ 
+ @return Whether succeed.
+ */
+- (BOOL)yy_modelSetWithDictionary:(NSDictionary *)dic;
+
+/**
+ Generate a json object from the receiver's properties.
+
+ @return A json object in `NSDictionary` or `NSArray`, or nil if an error occurs.
+ See [NSJSONSerialization isValidJSONObject] for more information.
+ 
+ @discussion Any of the invalid property is ignored.
+ If the reciver is `NSArray`, `NSDictionary` or `NSSet`, it just convert
+ the inner object to json object
+ */
+- (nullable id)yy_modelToJSONObject;
+
+/**
+ Generate a json string's data from the receiver's properties.
+
+ @return A json string's data, or nil if an error occurs.
+ 
+ @discussion Any of the invalid property is ignored.
+ If the reciver is `NSArray`, `NSDictionary` or `NSSet`, it will also convert the
+ inner object to json string.
+ */
+- (nullable NSData *)yy_modelToJSONData;
+
+/**
+ Generate a json string from the receiver's properties.
+
+ @return A json string, or nil if an error occurs.
+ 
+ @discussion Any of the invalid property is ignored.
+ If the reciver is `NSArray`, `NSDictionary` or `NSSet`, it will also convert the
+ inner object to json string.
+ */
+- (nullable NSString *)yy_modelToJSONString;
+
+/**
+ Copy a instance with the receiver's properties.
+
+ @return A copied instance, or nil if an error occurs.
+ */
+- (nullable id)yy_modelCopy;
+
+/**
+ Encode the receiver's properties to a coder.
+
+ @param aCoder An archiver object.
+ */
+- (void)yy_modelEncodeWithCoder:(NSCoder *)aCoder;
+
+/**
+ Decode the receiver's properties from a decoder.
+
+ @param aDecoder  An archiver object.
+ @return self
+ */
+- (id)yy_modelInitWithCoder:(NSCoder *)aDecoder;
+
+/**
+ Get a hash code with the receiver's properties.
+
+ @return Hash code.
+ */
+- (NSUInteger)yy_modelHash;
+
+/**
+ Compares the receiver with another object for equality, based on properties.
+
+ @param model Another object
+ @return `YES` if the reciever is equal to the object, otherwise `NO`.
+ */
+- (BOOL)yy_modelIsEqual:(id)model;
+
+/**
+ Description method for debugging purposes based on properties.
+
+ @return A string that describes the contents of the receiver.
+ */
+- (NSString *)yy_modelDescription;
 @end
 
 NS_ASSUME_NONNULL_END
