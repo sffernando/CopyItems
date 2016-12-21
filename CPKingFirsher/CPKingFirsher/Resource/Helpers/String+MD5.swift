10fb8f554e3a170c1ab4c6f36e2a360ed1e6cba6
fernando <sucrezhang@qq.com>
2016-12-21 22:09:29 +0800

    methods in String+MD5.swift

--- CPKingFirsher/CPKingFirsher/Resource/Helpers/String+MD5.swift
+++ CPKingFirsher/CPKingFirsher/Resource/Helpers/String+MD5.swift
@@ -80,3 +80,32 @@ extension NSMutableData {
         append(arrayOfBytes, length: arrayOfBytes.count)
     }
 }
+
+protocol HashProtocol {
+    var message: Array<UInt8> { get }
+        
+    /** Common part for hash calculation. Prepare header data. */
+    func prepare(_ len: Int) -> Array<UInt8>
+}
+
+extension HashProtocol {
+    
+    func prepare(_ len: Int) -> Array<UInt8> {
+        var tmpMessage = message
+        
+        // Step 1. Append Padding Bits
+        tmpMessage.append(0x80) // append one bit (UInt8 with one bit) to message
+        
+        // append "0" bit until message length in bits
+        var msgLength = tmpMessage.count
+        var counter = 0
+        
+        while msgLength % len != (len - 8) {
+            counter += 1
+            msgLength += 1
+        }
+        
+        tmpMessage += Array<UInt8>(repeating: 0, count: counter)
+        return tmpMessage;
+    }
+}
